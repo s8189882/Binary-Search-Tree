7943140e78f4c3fe4ea17db1e98102fc0c8c91b6
s8189882 <s8189882@gmail.com>
2022-09-10 13:49:56 +0530

    Add UC2 - Add the entire binary tree and verify nodes using size method

--- Binary Tree/src/binary/tree/through/linkedlist/BinaryTreeMain.java
+++ Binary Tree/src/binary/tree/through/linkedlist/BinaryTreeMain.java
@@ -3,12 +3,22 @@ package binary.tree.through.linkedlist;
 public class BinaryTreeMain {
 	
 	public static void main(String[] args) {
-		System.out.println("...Binary Tree Data Structure...");
+		System.out.println("Welcome to Binary Tree Data Structure!");
 		
 		BinaryTree<Integer> binaryTree = new BinaryTree<Integer>();
 		binaryTree.add(56);
 		binaryTree.add(30);
 		binaryTree.add(70);
+		binaryTree.add(22);
+		binaryTree.add(40);
+		binaryTree.add(60);
+		binaryTree.add(95);
+		binaryTree.add(11);
+		binaryTree.add(65);
+		binaryTree.add(3);
+		binaryTree.add(16);
+		binaryTree.add(63);
+		binaryTree.add(67);
 
 		System.out.println("\nBinary Tree Size = " + binaryTree.getSize() + "\n");
 		binaryTree.printTree();
