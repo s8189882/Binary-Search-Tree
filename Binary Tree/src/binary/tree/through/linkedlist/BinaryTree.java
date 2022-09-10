11dec03284a6aa2d9ed025fc471d711c69497838
s8189882 <s8189882@gmail.com>
2022-09-10 13:20:08 +0530

    Create master branch

---
+++ Binary Tree/src/binary/tree/through/linkedlist/BinaryTree.java
@@ -0,0 +1,9 @@
+package binary.tree.through.linkedlist;
+
+public class BinaryTree {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Binary Tree Data Structure!");
+	}
+}
